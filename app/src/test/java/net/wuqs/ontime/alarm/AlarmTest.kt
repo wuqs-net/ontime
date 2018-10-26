5f2db8b8f66256155e8f12d0fd5652f1c419c241
trifirew <wuqswuqs2001@gmail.com>
2018-10-26 17:18:10 -0400

    Updated test cases for alarm calculations
    Test cases no longer depend on real current time. A defined time is
    passed to the functions as the dummy "current" time.

--- app/src/test/java/net/wuqs/ontime/alarm/AlarmTest.kt
+++ app/src/test/java/net/wuqs/ontime/alarm/AlarmTest.kt
@@ -3,196 +3,131 @@ package net.wuqs.ontime.alarm
 import net.wuqs.ontime.db.Alarm
 import org.junit.Test
 import java.util.*
+import java.util.Calendar.*
 
 class AlarmTest {
 
-    val alarm = Alarm()
-    val next = Calendar.getInstance().apply { set(Calendar.MILLISECOND, 0) }!!
-
-    @Test
-    fun testRepeatDaily() {
-        //        alarm.apply {
-//            hour = 9
-//            minute = 0
-//            repeatType = Alarm.REPEAT_DAILY
-//            repeatCycle = 2
-//            repeatIndex = 0
-//            activateDate = Calendar.getInstance().apply {
-//                set(2018, 4, 8, hour, minute, 0)
-//                set(Calendar.MILLISECOND, 0)
-//            }
-//            getNextOccurrence()
-//        }
-//        next.apply {
-//            set(2018, 4, 12, 9, 0, 0)
-//            set(Calendar.MILLISECOND, 0)
-//        }
-//        println(alarm.nextTime!!.time)
-//        assert(next.compareTo(alarm.nextTime!!) == 0)
-//
-//        alarm.apply {
-//            hour = 4
-//            minute = 0
-//            repeatType = Alarm.REPEAT_DAILY
-//            repeatCycle = 2
-//            repeatIndex = 0
-//            activateDate = Calendar.getInstance().apply {
-//                set(2018, 2, 1, hour, minute, 0)
-//                set(Calendar.MILLISECOND, 0)
-//            }
-//            getNextOccurrence()
-//        }
-//        next.apply {
-//            set(2018, 4, 12, 4, 0, 0)
-//            set(Calendar.MILLISECOND, 0)
-//        }
-//        println(alarm.nextTime!!.time)
-//        assert(next.compareTo(alarm.nextTime!!) == 0)
-//
-//        alarm.apply {
-//            hour = 1
-//            minute = 0
-//            repeatType = Alarm.REPEAT_DAILY
-//            repeatCycle = 1
-//            repeatIndex = 0
-//            activateDate = Calendar.getInstance().apply {
-//                set(1970, 1, 1, hour, minute, 0)
-//                set(Calendar.MILLISECOND, 0)
-//            }
-//            getNextOccurrence()
-//        }
-//        next.apply {
-//            set(2018, 4, 11, 1, 0, 0)
-//            set(Calendar.MILLISECOND, 0)
-//        }
-//        println(alarm.nextTime!!.time)
-//        assert(next.compareTo(alarm.nextTime!!) == 0)
-
-//        alarm.apply {
-//            hour = 20
-//            minute = 8
-//            repeatType = Alarm.REPEAT_DAILY
-//            repeatCycle = 1
-//            repeatIndex = 0
-//            activateDate = Calendar.getInstance().apply {
-//                set(2018, 4, 11, 0, 0, 0)
-//                set(Calendar.MILLISECOND, 0)
-//            }
-//            getNextOccurrence()
-//        }
-//        next.apply {
-//            set(2018, Calendar.MAY, 13, 20, 8, 0)
-//            set(Calendar.MILLISECOND, 0)
-//        }
-//        println(alarm.nextTime!!.time)
-//        assert(next.compareTo(alarm.nextTime!!) == 0)
+    /**
+     * Asserts that the `expected` time and the `actual` time are the same.
+     */
+    private fun assertSameTime(expected: Calendar?, actual: Calendar?) {
+        assert(expected == actual) { "expected: ${expected?.time}, actual: ${actual?.time}" }
     }
 
-    @Test
-    fun testRepeatWeekly() {
-        alarm.apply {
-            hour = 23
-            minute = 41
-            repeatType = Alarm.REPEAT_WEEKLY
-            repeatCycle = 2
-            repeatIndex = 0b1000010 + (2 shl 8)
-            activateDate = GregorianCalendar(2018, Calendar.AUGUST, 12)
-            nextTime = getNextOccurrence()
-        }
-        next.apply {
-            set(2018, Calendar.AUGUST, 20, alarm.hour, alarm.minute, 0)
-        }
-        println("activate: ${alarm.activateDate?.time}, next: ${alarm.nextTime?.time}")
-        assert(next == alarm.nextTime)
+    private fun createMonthlyRepeatIndex(vararg days: Int) = days.sumBy { 1 shl (it - 1) }
 
-        alarm.apply {
-            hour = 23
-            minute = 41
-            repeatType = Alarm.REPEAT_WEEKLY
-            repeatCycle = 1
-            repeatIndex = 0b1000010 + (1 shl 8)
-            activateDate = GregorianCalendar(2018, Calendar.AUGUST, 12)
-            nextTime = getNextOccurrence()
-        }
-        next.apply {
-            set(2018, Calendar.AUGUST, 18, alarm.hour, alarm.minute, 0)
-        }
-        println("activate: ${alarm.activateDate?.time}, next: ${alarm.nextTime?.time}")
-        assert(next == alarm.nextTime)
+    private fun createWeeklyRepeatIndex(firstDay: Int, vararg days: Int): Int {
+        return days.sumBy { 1 shl (it - 1) } + (firstDay shl 8)
     }
 
+    // Test if the calculation is correct when current time is exactly when the alarm fires.
     @Test
-    fun testRepeatMonthly() {
-        alarm.apply {
-            hour = 23
-            minute = 41
-            repeatType = Alarm.REPEAT_MONTHLY_BY_DATE
-            repeatCycle = 1
-            repeatIndex = 0b111111111111000010
-            activateDate = GregorianCalendar(2018, Calendar.AUGUST, 12)
-            nextTime = getNextOccurrence()
-        }
-        next.apply {
-            set(2018, Calendar.AUGUST, 18, alarm.hour, alarm.minute, 0)
-        }
-        println("activate: ${alarm.activateDate?.time}, next: ${alarm.nextTime?.time}")
-        assert(next == alarm.nextTime)
+    fun testRepeatDaily1() {
+        val now = GregorianCalendar(2018, OCTOBER, 25, 9, 0)
+        val alarm = Alarm(
+                hour = 9,
+                minute = 0,
+                repeatType = Alarm.REPEAT_DAILY,
+                repeatCycle = 1,
+                activateDate = GregorianCalendar(2018, OCTOBER, 25)
+        )
+        val next = GregorianCalendar(2018, OCTOBER, 26, 9, 0)
+        assertSameTime(next, alarm.getNextOccurrence(now))
     }
 
+    // Test if the calculation is correct when the first day of week is not Sunday.
     @Test
-    fun updateNextOccurrence() {
-//        alarm.apply {
-//            hour = 23
-//            minute = 41
-//            repeatType = Alarm.REPEAT_MONTHLY_BY_DATE
-//            repeatCycle = 1
-//            repeatIndex = 0b101
-//            activateDate = Calendar.getInstance().apply {
-//                set(1950, Calendar.NOVEMBER, 4, 0, 0, 0)
-//                set(Calendar.MILLISECOND, 0)
-//            }
-//            nextTime = getNextOccurrence()
-//        }
-//        next.apply {
-//            set(2018, Calendar.AUGUST, 3, alarm.hour, alarm.minute, 0)
-//        }
-//        println("activate: ${alarm.activateDate?.time}, next: ${alarm.nextTime?.time}")
-//        assert(next == alarm.nextTime)
-//
-//        alarm.apply {
-//            hour = 23
-//            minute = 41
-//            repeatType = Alarm.REPEAT_MONTHLY_BY_DATE
-//            repeatCycle = 2
-//            repeatIndex = 0b111
-//            activateDate = Calendar.getInstance().apply {
-//                set(2018, Calendar.AUGUST, 4, 0, 0, 0)
-//                set(Calendar.MILLISECOND, 0)
-//            }
-//            nextTime = getNextOccurrence()
-//        }
-//        next.apply {
-//            set(2018, Calendar.OCTOBER, 1, alarm.hour, alarm.minute, 0)
-//        }
-//        println("activate: ${alarm.activateDate?.time}, next: ${alarm.nextTime?.time}")
-//        assert(next == alarm.nextTime)
+    fun testRepeatWeekly1() {
+        val now = GregorianCalendar(2018, OCTOBER, 26, 9, 0)
+        val alarm = Alarm(
+                hour = 8,
+                minute = 0,
+                repeatType = Alarm.REPEAT_WEEKLY,
+                repeatCycle = 2,
+                repeatIndex = createWeeklyRepeatIndex(MONDAY, MONDAY, SUNDAY),
+                activateDate = GregorianCalendar(2018, OCTOBER, 25)
+        )
+        val next = GregorianCalendar(2018, OCTOBER, 28, 8, 0)
+        assertSameTime(next, alarm.getNextOccurrence(now))
+    }
 
-        alarm.apply {
-            hour = 23
-            minute = 41
-            repeatType = Alarm.REPEAT_MONTHLY_BY_DATE
-            repeatCycle = 2
-            repeatIndex = 0b100000000001
-            activateDate = Calendar.getInstance().apply {
-                set(2017, Calendar.SEPTEMBER, 4, 0, 0, 0)
-                set(Calendar.MILLISECOND, 0)
-            }
-            nextTime = getNextOccurrence(GregorianCalendar(2018, Calendar.AUGUST, 5))
-        }
-        next.apply {
-            set(2018, Calendar.SEPTEMBER, 1, alarm.hour, alarm.minute, 0)
-        }
-        println("activate: ${alarm.activateDate?.time}, next: ${alarm.nextTime?.time}")
-        assert(next == alarm.nextTime)
+    // Test if the calculation is correct when current time is exactly when the alarm fires.
+    @Test
+    fun testRepeatWeekly2() {
+        val now = GregorianCalendar(2018, OCTOBER, 25, 9, 0)
+        val alarm = Alarm(
+                hour = 9,
+                minute = 0,
+                repeatType = Alarm.REPEAT_WEEKLY,
+                repeatCycle = 1,
+                repeatIndex = 0b0111110 + (1 shl 8),
+                activateDate = GregorianCalendar(2018, OCTOBER, 25)
+        )
+        val next = GregorianCalendar(2018, OCTOBER, 26, 9, 0)
+        assertSameTime(next, alarm.getNextOccurrence(now))
+    }
+
+    // Test if the calculation is correct when there are multiple dates in a month on which the
+    // alarm needs to fire on.
+    @Test
+    fun testRepeatMonthly1() {
+        val now = GregorianCalendar(2018, OCTOBER, 26, 9, 0)
+        val alarm = Alarm(
+                hour = 23,
+                minute = 45,
+                repeatType = Alarm.REPEAT_MONTHLY_BY_DATE,
+                repeatCycle = 2,
+                repeatIndex = createMonthlyRepeatIndex(25, 27, 29),
+                activateDate = GregorianCalendar(2018, OCTOBER, 25)
+        )
+        val next = GregorianCalendar(2018, OCTOBER, 27, 23, 45)
+        assertSameTime(next, alarm.getNextOccurrence(now))
+    }
+
+    // Test if the calculation is correct when current time is exactly when the alarm fires.
+    @Test
+    fun testRepeatMonthly2() {
+        val now = GregorianCalendar(2018, OCTOBER, 25, 9, 0)
+        val alarm = Alarm(
+                hour = 9,
+                minute = 0,
+                repeatType = Alarm.REPEAT_MONTHLY_BY_DATE,
+                repeatCycle = 1,
+                repeatIndex = createMonthlyRepeatIndex(25, 26, 27),
+                activateDate = GregorianCalendar(2018, OCTOBER, 25)
+        )
+        val next = GregorianCalendar(2018, OCTOBER, 26, 9, 0)
+        assertSameTime(next, alarm.getNextOccurrence(now))
+    }
+
+    // Test if the calculation is correct when current time is exactly when the alarm fires.
+    @Test
+    fun testRepeatMonthly3() {
+        val now = GregorianCalendar(2018, OCTOBER, 27, 9, 0)
+        val alarm = Alarm(
+                hour = 9,
+                minute = 0,
+                repeatType = Alarm.REPEAT_MONTHLY_BY_DATE,
+                repeatCycle = 1,
+                repeatIndex = createMonthlyRepeatIndex(25, 26, 27),
+                activateDate = GregorianCalendar(2018, OCTOBER, 25)
+        )
+        val next = GregorianCalendar(2018, NOVEMBER, 25, 9, 0)
+        assertSameTime(next, alarm.getNextOccurrence(now))
+    }
+
+    // Test if the calculation is correct when current time is exactly when the alarm fires.
+    @Test
+    fun testRepeatYearly1() {
+        val now = GregorianCalendar(2018, OCTOBER, 25, 9, 0)
+        val alarm = Alarm(
+                hour = 9,
+                minute = 0,
+                repeatType = Alarm.REPEAT_YEARLY_BY_DATE,
+                repeatCycle = 1,
+                activateDate = GregorianCalendar(2018, OCTOBER, 25)
+        )
+        val next = GregorianCalendar(2019, OCTOBER, 25, 9, 0)
+        assertSameTime(next, alarm.getNextOccurrence(now))
     }
 }
