cd6342446dc1d590014ef2733a008bb8f2e05b68
trifirew <wuqswuqs2001@gmail.com>
2019-01-04 14:36:47 -0500

    Add "rate this app" in About screen
    User will be redirected to market page to rate this app.

--- app/src/main/java/net/wuqs/ontime/feature/about/AboutActivity.kt
+++ app/src/main/java/net/wuqs/ontime/feature/about/AboutActivity.kt
@@ -1,10 +1,15 @@
 package net.wuqs.ontime.feature.about
 
+import android.content.ActivityNotFoundException
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_about.*
 import net.wuqs.ontime.BuildConfig
 import net.wuqs.ontime.R
+import net.wuqs.ontime.util.logE
+import net.wuqs.ontime.util.logV
 
 class AboutActivity : AppCompatActivity() {
 
@@ -13,5 +18,18 @@ class AboutActivity : AppCompatActivity() {
         setContentView(R.layout.activity_about)
 
         tv_app_version.text = BuildConfig.VERSION_NAME
+
+        tv_review.setOnClickListener { rate() }
+    }
+
+    private fun rate() {
+        try {
+            val uri = Uri.parse("market://details?id=${BuildConfig.APPLICATION_ID}")
+            logV("Opening $uri")
+            val intent = Intent(Intent.ACTION_VIEW, uri)
+            startActivity(intent)
+        } catch (e: ActivityNotFoundException) {
+            logE("Cannot open market page for OnTime")
+        }
     }
 }
